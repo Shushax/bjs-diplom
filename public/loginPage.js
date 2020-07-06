82aac9e29af17107c6994e55f1d26d3e75ab8726
Shushax <nekfi@mail.ru>
2020-07-06 17:50:45 +0300

    Небольшие правки

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,7 +1,9 @@
 "use strict";
 const login = new UserForm();
-login.loginFormCallback = (data) => { ApiConnector.login(data, callback)
+login.loginFormCallback = (data) => ApiConnector.login(data, callback);
 if (!callback) {
     login.setLoginErrorMessage("Ошибка при авторизации!");
+} else {
+    location.reload();
 }
-}
+
