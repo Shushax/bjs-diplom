debaeb367a2fa89580e5333fb26a4bc8d0708c5c
Shushax <nekfi@mail.ru>
2020-07-07 22:42:08 +0300

    Добавлено пополнение счета, есть проблема с сообщением

--- public/homePage.js
+++ public/homePage.js
@@ -21,3 +21,14 @@ function rate() {
     });
 }
 rate();
+setInterval(rate(), 60000);
+
+const money = new MoneyManager();
+money.addMoneyCallback = (data) => ApiConnector.addMoney(data, () => {
+    if (data.amount && data.currency) {
+        ProfileWidget.showProfile(data);
+        money.setMessage(isError, "Баланс успешно пополнен!");
+        document.location.reload(true);
+    }
+});
+
