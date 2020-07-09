802fe7f47bdd970a786c70510c2c671482302818
Shushax <nekfi@mail.ru>
2020-07-09 18:17:33 +0300

    Исправлены цвета ошибок

--- public/homePage.js
+++ public/homePage.js
@@ -27,27 +27,27 @@ const money = new MoneyManager();
 money.addMoneyCallback = (data) => ApiConnector.addMoney(data, (responce) => {
     if (responce.success === true) {
         ProfileWidget.showProfile(responce.data);
-        money.setMessage(true, "Баланс успешно пополнен!");
+        money.setMessage(false, "Баланс успешно пополнен!");
     } else {
-        money.setMessage(false, "Ошибка при пополнении счета!")
+        money.setMessage(true, "Ошибка при пополнении счета!")
     }
 });
 
 money.conversionMoneyCallback = (data) => ApiConnector.convertMoney(data, (responce) => {
     if (responce.success === true) {
         ProfileWidget.showProfile(responce.data);
-        money.setMessage(true, "Валюта успешно конвертирована!");
+        money.setMessage(false, "Валюта успешно конвертирована!");
     } else {
-        money.setMessage(false, "Ошибка при конвертировании валюты!")
+        money.setMessage(true, "Ошибка при конвертировании валюты!")
     }
 });
 
 money.sendMoneyCallback = (data) => ApiConnector.transferMoney(data, (responce) => {
     if (responce.success === true) {
         ProfileWidget.showProfile(responce.data);
-        money.setMessage(true, "Валюта успешно переведена!");
+        money.setMessage(false, "Валюта успешно переведена!");
     } else {
-        money.setMessage(false, "Ошибка при переводе валюты!")
+        money.setMessage(true, "Ошибка при переводе валюты!")
     }
 });
 
@@ -65,9 +65,9 @@ favorites.addUserCallback = (data) => ApiConnector.addUserToFavorites(data, (res
         favorites.clearTable();
         favorites.fillTable(responce.data);
         money.updateUsersList(responce.data);
-        favorites.setMessage(true, "Пользователь успешно добавлен!");
+        favorites.setMessage(false, "Пользователь успешно добавлен!");
     } else {
-        favorites.setMessage(false, "При добавлении пользователя произошла ошибка!");
+        favorites.setMessage(true, "При добавлении пользователя произошла ошибка!");
     }
 });
 
@@ -76,8 +76,8 @@ favorites.removeUserCallback = (data) => ApiConnector.removeUserFromFavorites(da
         favorites.clearTable();
         favorites.fillTable(responce.data);
         money.updateUsersList(responce.data);
-        favorites.setMessage(true, "Пользователь успешно удален!");
+        favorites.setMessage(false, "Пользователь успешно удален!");
     } else {
-        favorites.setMessage(false, "При удалении пользователя произошла ошибка!");
+        favorites.setMessage(true, "При удалении пользователя произошла ошибка!");
     }
 });
